5713669e2c4401394b6e558e379a3e50c3e9f52b
Víctor López Ferrando <victorlopez90@gmail.com>
2018-03-07 11:27:24 +0100

    afegits alguns pdfs d'enunciats

--- imatges_enunciats.py
+++ imatges_enunciats.py
@@ -17,39 +17,32 @@ parser = argparse.ArgumentParser()
 parser.add_argument('pdf_enunciats')
 args = parser.parse_args()
 
-marge_esquerre = 0
 
-def calcula_marges(y, pixels, width):
-    primer = pixels[y].index(0)
-    ultim = (len(pixels[y]) - 1) - pixels[y][::-1].index(0)
+def obte_linies(pixels):
+    # Get line with first black pixel
+    y = next(i for i, line in enumerate(pixels) if line.count(0) > 0)
+    l = [y-10 if y-10 >= 0 else 0]
 
-    global marge_esquerre
-    marge_esquerre = primer
-
-def es_linia(y, pixels, width):
-    n_negres = pixels[y].count(0)
-    p_negres = float(n_negres) / float(width)
-    return p_negres > 0.8
-
-def obte_linies(pixels, width, height):
-    l = []
     y = 0
-    while y < height:
-        if es_linia(y, pixels, width):
+    while y < len(pixels):
+        if float(pixels[y].count(0)) / len(pixels[y]) > 0.8:
+            marge_esquerre = pixels[y].index(0)
             l.append(y)
             y += 10
         else:
             y += 1
-    calcula_marges(l[0], pixels, width)
-    return l
 
-SECCIONS_IGNORAR = [1, 12, 23]
+    # Get line with last black pixel
+    y = next(i for i, line in reversed(list(enumerate(pixels))) if line.count(0) > 0)
+    l.append(y+15 if y+15 < len(pixels) else len(pixels))
+    return l, marge_esquerre
+
+SECCIONS_IGNORAR = [1, 2, 13, 24]
 seccio = 0
 questio = 0
 
 # Per cadascuna de les 4 pàgines
 for pagina in range(4):
-    # TODO: use temporary file
     # Convertir a png
     os.system('convert -density 150 %s[%d] enunciats.png' % (
         args.pdf_enunciats, pagina))
@@ -60,13 +53,17 @@ for pagina in range(4):
     bw = gray.point(lambda x: 0 if x<128 else 255, '1')
     pixels = list(bw.getdata())
     width, height = image.size
-    pixels = [pixels[i*width: (i+1)*width] for i in range(height)]
+    pixels = [pixels[i*width:(i+1)*width] for i in range(height)]
 
     # Troba les posicions de les línies
-    linies = obte_linies(pixels, width, height)
+    linies, marge_esquerre = obte_linies(pixels)
 
     # Retallar i guardar les imatges
     for y1, y2 in zip(linies, linies[1:]):
+        # Ignorar seccions buides
+        if sum([linia.count(0) for linia in pixels[y1+5:y2-10]]) == 0:
+            continue
+
         seccio += 1
         if seccio in SECCIONS_IGNORAR:
             continue
@@ -74,7 +71,7 @@ for pagina in range(4):
         box = (marge_esquerre, y1+5, width - marge_esquerre, y2-10)
         crop = image.crop(box)
         crop.save('%02d.png' % questio)
-        os.system('optipng %02d.png' % questio)
+        #os.system('optipng %02d.png' % questio)
 
 os.system('rm -f enunciats.png')
 
